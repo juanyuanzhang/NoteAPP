0a37aa0a8fd59ba5c56b9476840ce80dabc0473e
yuanjuan <juanyuan.zhang@gmail.com>
2018-07-04 17:34:15 +0800

    2018.7.4  note v1

---
+++ app/src/main/java/com/example/administrator/noteapp/MainActivity.java
@@ -0,0 +1,88 @@
+package com.example.administrator.noteapp;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+
+public class MainActivity extends AppCompatActivity {
+    private Intent i;
+    private ListView listView;
+    private MDBAdapter mdbAdapter;
+    private SimpleCursorAdapter dataAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView = findViewById(R.id.mainList);
+        mdbAdapter = new MDBAdapter(this);
+        showlistView();
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Cursor itemcursor = (Cursor) listView.getItemAtPosition(position);
+                int itemid = itemcursor.getInt( itemcursor.getColumnIndexOrThrow("_id"));
+                i = new Intent();
+                i.putExtra("itemid",itemid);
+                i.putExtra("key","edit");
+                i.setClass(MainActivity.this,AddActivity.class);
+                startActivity(i);
+            }
+        });
+
+
+
+
+    }
+    public void showlistView(){
+        Cursor cursor = mdbAdapter.listshow();
+        String[] columns = new String[]{
+                mdbAdapter.KEY_DATE,
+                mdbAdapter.KEY_TOP,
+                mdbAdapter.KEY_CONT
+
+
+        };
+        int[] to = new int[]{
+                R.id.tvdata,//list_item
+                R.id.tvnote,
+                R.id.tvcon
+
+        };
+        // 自訂的Adapter 需要使用SimpleCursorAdapter   來取資料放入listView中
+        dataAdapter = new SimpleCursorAdapter(this,R.layout.listitem, cursor, columns, to, 0);
+        listView.setAdapter(dataAdapter);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.toolbr,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent intent = new Intent(this,AddActivity.class);
+        switch (item.getItemId()){
+            case R.id.baradd:
+                intent.putExtra("key","add");
+                startActivity(intent);
+                break;
+//            case R.id.baredit:
+//                intent.putExtra("key","edit");
+//                startActivity(intent);
+//                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
