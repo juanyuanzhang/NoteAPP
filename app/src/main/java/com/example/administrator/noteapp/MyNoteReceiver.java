3a33462336f5272aae0d55ac34b81ae321c98ffc
juanyuanzhang <40617047+juanyuanzhang@users.noreply.github.com>
2018-08-13 14:52:05 +0800

    [add] 增加提醒震動功能
    -AndroidManifest.xml加入震動權限
    -MyNoteReceiver.java加入震動程式碼
    
    20180813

--- app/src/main/java/com/example/administrator/noteapp/MyNoteReceiver.java
+++ app/src/main/java/com/example/administrator/noteapp/MyNoteReceiver.java
@@ -3,9 +3,11 @@ package com.example.administrator.noteapp;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Vibrator;
 import android.util.Log;
 
 import java.util.Calendar;
@@ -13,10 +15,13 @@ import java.util.Calendar;
 import static android.content.Context.NOTIFICATION_SERVICE;
 
 public class MyNoteReceiver extends BroadcastReceiver {
-
     //利用廣播開啟提醒通知
     @Override
     public void onReceive(Context context, Intent intent) {
+        Vibrator myVibrator;//宣告震動物件變數 ，使用震動必須宣告權限喔
+        myVibrator = (Vibrator) context.getSystemService(Service.VIBRATOR_SERVICE);//取得震動物件
+        if(myVibrator!=null)
+        myVibrator.vibrate(new long[]{500, 100, 500, 100, 500, 100}, -1);//參數1是振動0.5秒停0.1秒連續三次.參數2是-1代表只 震動一次
         //設定通知的附加動作
         Intent i =new Intent(context,MainActivity.class);
         PendingIntent  pendingIntent = PendingIntent.getActivity(context,0,i,PendingIntent.FLAG_UPDATE_CURRENT);
